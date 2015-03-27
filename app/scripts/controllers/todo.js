7308975ec7641bbfae706fa2efc335574025fdce
Felipe Millan <hello@fmillan.com>
2015-03-27 14:50:49 +0700

    ToDo Project Done

--- app/scripts/controllers/todo.js
+++ app/scripts/controllers/todo.js
@@ -5,6 +5,15 @@ angular.module('todoblocApp')
     // synchronized array of todos
     $scope.todos = fbtodo.syncArray('todos');
 	var Done = $scope.todos.Done;
+  // loop through the todos list and update the todo is expired
+    for(var i=0;i<$scope.todos.length;i++){
+              var is_expired = $scope.todoExpired(todos[i]);
+                if (is_expired && todos[i].Expired == false) { 
+                  // need to update for Expired date here 
+                  $scope.todos[i].Expired = true;
+                  $scope.todos.$save(i);
+                }
+          }
 	
 	// provide a method for adding a todo
 	$scope.addTodo = function(newTodo, todoPriority, status) {
@@ -33,7 +42,6 @@ angular.module('todoblocApp')
 	    }
     };
 	
-	
 	$scope.changeState = function(key, done) {
 	    for(var i=0;i<$scope.todos.length;i++){
 	        if(key==$scope.todos[i].$id){
@@ -52,24 +60,28 @@ angular.module('todoblocApp')
 	        if(key==$scope.todos[i].$id){
 	            if (done === false ) {
 						$scope.todos[i].Expired = true;
+                  
 			    } else {
+                  // when change this to false,
+                  // we also have to update todo.date to new date to avoid it be automatic make expired when app initialize
+                  
 			      $scope.todos[i].Expired = false;
+                  $scope.todos[i].Date =  Date.now() / 1000;
 			    }
 			    $scope.todos.$save(i);
 	        }
 	    }
     };
 	
-
 	$scope.todoExpired = function(todo) { 
-		  var today = new Date();
-		  var fDate = new Date(todo.date);
-		  fDate.setDate(fDate.getDate() + 7);
-		    if (today > fDate) {
-		      return true;
-		        } else {
-		      return false;
-	    }
-	};
-
+      // get today date
+	  var the_7_days_before = new Date();
+		the_7_days_before.setDate(today.getDate()-7);
+	      if ((the_7_days_before.getTime()/1000) > todo.date ) {
+	        return true;
+	      }
+	      else {
+	        return false;
+	      }
+      	};
 });
